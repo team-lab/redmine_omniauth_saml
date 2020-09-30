f726aecc86fdb93ed98270c0a0b531132a3249cb
kikebe <ikebe@team-lab.com>
2020-09-30 19:15:12 +0900

    patch for onelogin

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ RedmineApp::Application.routes.draw do
   match '/auth/:provider'           => 'account#login_with_saml_redirect',  as: :sign_in, via: [:get, :post]
   match '/auth/:provider/sls'       => 'account#redirect_after_saml_logout', as: :sign_out, via: [:get, :post]
   match '/saml/metadata'            => 'saml#metadata', via: [:get]
+  post  '/auth/:provider/consume'   => redirect { "/auth/saml" }
 end
